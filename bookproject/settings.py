d9284b8206b1b75e4688a4aeda1c9a39908f297e
Mai23944 <g23944sm@gm.tsuda.ac.jp>
2025-05-26 23:22:33 +0900

    second

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
 if not DEBUG:
     DATABASES = {
-        'default' : dj_databases_url.config(
+        'default' : dj_database_url.config(
             # Replace this value with your local database's connection string.
             default='postgresql://postgres:postgres@localhost:5432/bookproject',
             conn_max_age=600
